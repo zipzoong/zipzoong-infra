0df2c48fe75d33f6a22e7cf9b69023904aac65cd
industriously <dev.rojiwon@gamil.com>
2023-06-20 22:29:03 +0900

    policy update

--- resources/zipzoong-storage/datas.tf
+++ resources/zipzoong-storage/datas.tf
@@ -3,19 +3,22 @@ data "aws_iam_role" "backend_main" {
 }
 
 data "aws_iam_policy_document" "public_policy" {
+  version = "2012-10-17"
   statement {
-    effect  = "Allow"
-    actions = ["s3:GetObject"]
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = [modoule.s3_public.aws_s3_bucket.this.arn]
     principals {
       type        = "*"
       identifiers = ["*"]
     }
   }
   statement {
-    effect  = "Allow"
-    actions = ["s3:PutObject"]
+    effect    = "Allow"
+    actions   = ["s3:PutObject"]
+    resources = [modoule.s3_public.aws_s3_bucket.this.arn]
     principals {
-      type        = "AWS"
+      type        = "Service"
       identifiers = [data.aws_iam_role.backend_main.arn]
     }
   }
