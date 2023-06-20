56c5d1e79db83200c0d3468f442f2481455bff1c
industriously <dev.rojiwon@gamil.com>
2023-06-21 03:36:03 +0900

    bugfix

--- modules/s3/datas.tf
+++ modules/s3/datas.tf
@@ -2,18 +2,18 @@ data "aws_iam_policy_document" "this" {
   count   = length(var.policy_statements) > 0 ? 1 : 0
   version = "2012-10-17"
   dynamic "statement" {
-    for_each = var.policy_statements
-    iterator = "statement"
+    for_each = toset(var.policy_statements)
+
     content {
       effect    = statement.allow ? "Allow" : "Deny"
-      actions   = statement.value.actions
-      resources = [for path in toset(statement.value.resource_paths) : "${aws_s3_bucket.this.arn}/${path}"]
+      actions   = statement.actions
+      resources = [for path in toset(statement.resource_paths) : "${aws_s3_bucket.this.arn}/${path}"]
       dynamic "principals" {
         for_each = statement.principals
-        iterator = "principal"
+
         content {
-          type        = principal.key
-          identifiers = principal.value
+          type        = principals.key
+          identifiers = principals.value
         }
       }
     }
