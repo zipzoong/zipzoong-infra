68db02d465a1395b2095d825fa672a690389acbf
industriously <dev.rojiwon@gamil.com>
2023-06-20 22:02:06 +0900

    bugfix

--- resources/zipzoong-storage/datas.tf
+++ resources/zipzoong-storage/datas.tf
@@ -8,14 +8,14 @@ data "aws_iam_policy_document" "public_policy" {
     actions = ["s3:GetObject"]
     principals {
       type        = "AWS"
-      identifiers = "*"
+      identifiers = ["*"]
     }
   }
   statement {
-    effect = "Allow"
+    effect  = "Allow"
     actions = ["s3:PutObject"]
     principals {
-      type ="AWS"
+      type        = "AWS"
       identifiers = data.aws_iam_role.backend_main.arn
     }
   }
