6382535eb8721abe10e3b25dc75769d913a5e082
industriously <dev.rojiwon@gamil.com>
2023-06-22 19:07:38 +0900

    bugfix

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -6,6 +6,7 @@ resource "aws_vpc" "this" {
   tags = {
     Name = "${var.project}-vpc"
   }
+  cidr_block           = "10.0.0.0/16"
   instance_tenancy     = "default"
   enable_dns_support   = true
   enable_dns_hostnames = true
