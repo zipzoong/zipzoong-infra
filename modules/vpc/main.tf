e6e8499329ff2a7e3755b32e411fc74f9a852dc7
industriously <dev.rojiwon@gamil.com>
2023-06-23 17:36:23 +0900

    파일명 변경

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -131,3 +131,39 @@ resource "aws_route_table_association" "private" {
   subnet_id      = element(aws_subnet.private[*].id, count.index)
   route_table_id = aws_route_table.private.id
 }
+
+resource "aws_default_security_group" "default" {
+  tags = {
+    Name = "${var.project}-sg-default"
+  }
+  vpc_id                 = aws_vpc.this.id
+  revoke_rules_on_delete = false
+}
+
+resource "aws_security_group" "this" {
+  count = length(var.sg)
+  tags = {
+    Name = join("-", [var.project, "sg", element(var.sg[*].name, count.index)])
+  }
+  vpc_id      = aws_vpc.this.id
+  name        = element(var.sg[*].name, count.index)
+  description = "allow ${element(var.sg[*].name, count.index)} inbound traffic"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [local.public_cidr]
+  }
+
+  dynamic "ingress" {
+    for_each = element(var.sg[*].inbound, count.index)
+
+    content {
+      from_port   = ingress.value.port
+      to_port     = ingress.value.port
+      protocol    = ingress.value.protocol
+      cidr_blocks = [local.public_cidr]
+    }
+  }
+}
--- resources/zipzoong-ecr/main.tf
+++ resources/zipzoong-ecr/main.tf
@@ -3,3 +3,9 @@ module "ecr_backend_main" {
   name    = "${local.service}_backend_main"
   kms_key = "${local.service}_production_kms"
 }
+
+module "ecr_web_main" {
+  source  = "../../modules/ecr"
+  name    = "${local.service}_web_main"
+  kms_key = "${local.service}_production_kms"
+}
