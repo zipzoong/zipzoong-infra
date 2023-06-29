8887d02ae29ae03f9e7c096f0ac0766fa7b51079
industriously <dev.rojiwon@gamil.com>
2023-06-29 22:45:03 +0900

    vpc

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -122,13 +122,13 @@ resource "aws_route_table" "private" {
 
 resource "aws_route_table_association" "public" {
   count          = length(aws_subnet.public)
-  subnet_id      = element(aws_subnet.public, count.index)
+  subnet_id      = element(aws_subnet.public[*].id, count.index)
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "private" {
-  for_each       = aws_subnet.private
-  subnet_id      = each.value.id
+  count          = length(aws_subnet.private)
+  subnet_id      = element(aws_subnet.private[*].id, count.index)
   route_table_id = aws_route_table.private.id
 }
 
